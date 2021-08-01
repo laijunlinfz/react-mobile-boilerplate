c1404fe6d8cfc7ddc70a87b3f91f74612016db88
laijunlinfz <993700855@qq.com>
2021-08-01 21:45:14 +0800

    style: tt1234

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import configureStore from '@/store';
 import reportWebVitals from './reportWebVitals';
 import './utils/lib/before-load';
 import './styles/index.less';
-// test 123
+// test 1234
 
 const store = configureStore();
 
